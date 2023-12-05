4c5ef35ccc0483fc1ee33d844845a1c133b6ed05
naveenareddypasam <149114756+naveenareddypasam@users.noreply.github.com>
2023-12-04 20:52:26 -0600

    Update submit.php

--- submit.php
+++ submit.php
@@ -1,12 +1,20 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $patientName = $_POST["patientName"];
-    $appointmentDate = $_POST["appointmentDate"];
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
 
-    // Here, you would typically connect to a database and store the data
-    // For demonstration, we will just display the data
+    // Here, you would typically process the data, like storing it in a database or sending an email
+    // For demonstration, we will just display the submitted data
 
-    echo "Patient Name: " . $patientName . "<br>";
-    echo "Appointment Date: " . $appointmentDate;
+    echo "<h1>Form Submission</h1>";
+    echo "<p>Name: " . htmlspecialchars($name) . "</p>";
+    echo "<p>Email: " . htmlspecialchars($email) . "</p>";
+    echo "<p>Message: " . nl2br(htmlspecialchars($message)) . "</p>";
+
+    // In a real application, you would redirect or inform the user of successful submission
+}
+else {
+    echo "Form submission error.";
 }
 ?>
